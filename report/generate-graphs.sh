56e43c282429105de6eb23fa7483e480fe25863e
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-12-07 22:34:59 +0100

    Add cavium graphs

--- report/generate-graphs.sh
+++ report/generate-graphs.sh
@@ -16,59 +16,64 @@ function gnuplot_box_threads() {
   # arg1: data file
   # arg2: output file
   # arg3: columns in the file
+  # arg4: cpu
+  case "$4" in
+    "laptop") xtics="('1' 1, '2' 2, '4' 3)" ;;
+    "ryzen") xtics="('1' 1, '2' 2, '4' 3, '8' 4, '16' 5)" ;;
+    "server") xtics="('1' 1, '2' 2, '4' 3, '8' 4, '12' 5)" ;;
+    "scaleway") xtics="('1' 1, '2' 2, '4' 3, '8' 4, '16' 5, '32' 6)" ;;
+    *)
+      (2>&1 echo "Invalid kind: $4")
+      ;;
+  esac
   gnuplot <<< "\
 set style fill solid 0.25 border -1
 set style boxplot outliers pointtype 7
 set style data boxplot
 set size square
-set xlabel 'Number of Threads'
-set ylabel 'Time (ns) for the benchmark'
-set xtics ('1' 1, '2' 2, '4' 3, '8' 4, '12' 5)
+set key font \",21\"
+set xtics $xtics
 set pointsize 0.3
 set terminal pdf size 10cm,10cm
+set termoption font \"Arial,16\"
 set output \"$2\"
 plot for [i=1:$3] \"$1\" using (i):i notitle
 "
 }
 
 # TODO: search for this dynamically
-benches="nop queue_pop queue_push queue_transfer list_remove"
+benches=`find "$DIR" | grep -oP "b:\K[a-zA-Z_]+" | sort | uniq`
 
 function plots_comparing_threads() {
   # Loop over the benchmark sets
   for kind in $(echo "$KINDS"); do
     case $kind in
-      "laptop")
-        threads=3
-        ;;
-      "ryzen")
-        threads=5
-        ;;
-      "server")
-        threads=5
-        ;;
+      "laptop") threads=3 ;;
+      "ryzen") threads=5 ;;
+      "server") threads=5 ;;
+      "scaleway") threads=6 ;;
       *)
         (2>&1 echo "that kind is not valid! $kind")
         exit 1
         ;;
     esac
+    DATADIR="$DIR/$kind/"
 
     # Loop over all benchmarks, since it doesn't make sense to have data from multiple benchmarks in a
     # single plot.
     for bench in $(echo "$benches"); do
       # The files matching this benchmark
-      data=`find $DIR -name "*$bench*" | sort`
+      data=`find $DATADIR -name "*$bench*" | sort`
       # All schemes that have this benchmark
       schemes=`echo "$data" | grep -oP "s:\K[a-zA-Z]*(?=-b:)" | uniq`
       # For each of the schemes, find its data files for this benchmark, and merge the thread data into
       # a single file.
       for scheme in $(echo "$schemes"); do
-        echo "kind=$kind bench=$bench scheme=$scheme"
         tmppath="$TMP/$kind-s:$scheme-b:$bench.data"
         outpath="$OUT/$kind-s:$scheme-b:$bench.pdf"
-        matchingfiles=`find $DIR -name "s:$scheme-b:$bench-*" | sort`
+        matchingfiles=`find $DATADIR -name "s:$scheme-b:$bench-*" | sort`
         paste $matchingfiles > "$tmppath"
-        gnuplot_box_threads "$tmppath" "$outpath" "$threads" "$scheme, $bench"
+        gnuplot_box_threads "$tmppath" "$outpath" "$threads" "$kind"
       done
     done
   done
