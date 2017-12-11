10d8d28010359bf691e143de7ab92b703ed78c41
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-12-11 03:05:15 +0100

    Make bar graph comparing against nothing as baseline.

--- report/generate-graphs.sh
+++ report/generate-graphs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# arg1: input directory to find the files from.
-# arg2: output file to which we put merged data files, and graphs.
-# arg3: number of threads in the data set.
 
 DIR="data"
 if [[ ! -d "$DIR" ]]; then mkdir "$DIR"; fi
@@ -31,7 +28,7 @@ function gnuplot_box_threads() {
 set style fill solid 0.25 border -1
 set style boxplot outliers pointtype 7
 set style data boxplot
-set size square
+# set size square
 set key font \",21\"
 set xtics $xtics
 set pointsize 0.3
@@ -42,7 +39,6 @@ plot for [i=1:$3] \"$1\" using (i):i notitle
 "
 }
 
-# TODO: search for this dynamically
 benches=`find "$DIR" | grep -oP "b:\K[a-zA-Z_]+" | sort | uniq`
 
 function plots_comparing_threads() {
@@ -76,9 +72,95 @@ function plots_comparing_threads() {
         matchingfiles=`find $DATADIR -name "s:$scheme-b:$bench-*" | sort`
         paste $matchingfiles > "$tmppath"
         gnuplot_box_threads "$tmppath" "$outpath" "$threads" "$kind"
+        echo "$kind $bench $scheme"
       done
     done
   done
 }
 
+function gnuplot_barplots() {
+  # arg1: input file
+  # arg2: output file
+  # arg3: number of threads in the bench
+  # arg4: thread labels
+  # arg5: scheme names
+  gnuplot <<< "\
+set style fill solid 0.25 border -1
+set style fill solid
+set style data boxes
+set boxwidth 0.8
+set xtics scale 0 ()
+threads=\"$4\"
+schemes=\"$5\"
+# set logscale y 10
+set key font \",21\"
+set terminal pdf size (10 + $3)cm,10cm
+set termoption font \"Arial,\"
+set output \"$2\"
+set for  [i=1:$3] xtics add (word(threads, i) i * ($3 + 2) - $3 * 0.75)
+plot for [i=2:$3] \"$1\" using (column(0) * ($3 + 2) + (i)):(column(i) -\
+  column(1)) title word(schemes, i-1)
+"
+}
+
+function avg_python() {
+  python <<< "
+lines = open(\"$1\").readlines()
+nums = map(lambda l: int(l), lines)
+print(sum(nums)/ len(lines))"
+}
+
+
+function relative_barplots() {
+  # Generate a relative clustered bar graph for all benchmarks. The baseline
+  # should be `nothing`. It should be a grouped barplot, in which one cluster
+  # has the same nubmer of threads, and the bars in the clusters are the
+  # schemes.
+  # one section for each kind
+  for kind in $(echo "$KINDS"); do
+    for bench in $(echo "$benches"); do
+      case $kind in
+        "laptop") n_threads=3  ; xtics="1 2 4" ;;
+        "ryzen") n_threads=5   ; xtics="1 2 4 8 16" ;;
+        "server") n_threads=5  ; xtics="1 2 4 8 12" ;;
+        "scaleway") n_threads=6; xtics="1 2 4 8 16 32" ;;
+        "gribb") n_threads=4   ; xtics="1 2 4 8" ;;
+        *)
+          (2>&1 echo "that kind is not valid! $kind")
+          exit 1
+          ;;
+      esac
+      # one graph for each bench
+      DATADIR="$DIR/$kind"
+      data=`find "$DATADIR" -name "*$bench*" | sort`
+      all_schemes=`echo "$data" | grep -oP "s:\K[a-zA-Z]*(?=-b:)" | sort | uniq`
+      echo "$all_schemes" | grep "nothing" >/dev/null
+      a="$?"
+      if [[ "$a" =~ 1 ]]; then
+        echo "[warn]: skipping $kind $bench, as we're missing 'nothing'."
+        continue
+      fi
+      schemes=`echo "$all_schemes" | grep -v "nothing"`
+      threads=`echo "$data" | grep -oP "t:\K\d{2}" | sort | uniq`
+
+      outputfile="$TMP/$kind-b:$bench.avg"
+      echo "" > "$outputfile"
+      for thread in $(echo "$threads"); do
+        for scheme in $(echo "nothing $schemes"); do
+          file="$DATADIR/s:$scheme-b:$bench-t:$thread"
+          printf "%d " $(avg_python "$file") >> "$outputfile"
+        done
+        printf "\n" >> "$outputfile"
+      done
+
+      outpath="$OUT/$kind-b:$bench.pdf"
+      schemes_oneline=`tr '\r\n' ' ' <<< "$schemes"`
+      gnuplot_barplots "$outputfile" "$outpath" "$n_threads"  "$xtics"\
+      "$schemes_oneline"
+      echo "$outpath"
+    done
+  done
+}
+
 plots_comparing_threads
+relative_barplots
